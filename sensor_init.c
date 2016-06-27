52a010742cec5f61226bb52d28c50df2a601c9cb
hugo chen <2482752117@qq.com>
2016-06-27 10:03:26 +0800

    bmp180 pressure ic driver
    measure  preussure using bmp180
    this is a C driver

---
+++ sensor_init.c
@@ -0,0 +1,19 @@
+#include "sensor_init.h"
+
+struct Sensor_protype sensor_pkg[14] =
+{
+    {"null      ",SENSOR_NULL,NULL}, 
+    {"relay     ",SENSOR_NULL,NULL},  
+    {"sht10     ",SENSOR_NULL,NULL},  
+    {"light     ",SENSOR_NULL,NULL}, 
+    {"infrared  ",SENSOR_NULL,NULL},  
+    {"hall      ",SENSOR_NULL,NULL},  
+    {"alcohol   ",SENSOR_NULL,NULL},  
+    {"pressure  ",SENSOR_NULL,NULL},  
+    {"rfid      ",SENSOR_NULL,NULL}, 
+    {"shock     ",SENSOR_NULL,NULL},  
+    {"rain      ",SENSOR_NULL,NULL}, 
+    {"ultrasonic",SENSOR_NULL,NULL},  
+    {"accel     ",SENSOR_NULL,NULL}, 
+		{"gas       ",SENSOR_NULL,NULL},
+};
